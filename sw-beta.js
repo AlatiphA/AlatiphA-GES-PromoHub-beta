6b6eda5947480e7002a3f38a6400242910bea003
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-06-09 08:52:24 +0000

    Bump app version from 1.1.4 to 1.1.5

--- sw-beta.js
+++ sw-beta.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION  = "1.1.4";
+const APP_VERSION  = "1.1.5";
 const CACHE_NAME   = `alatipha-ges-pasco-${APP_VERSION}`;
 
 const FILES_TO_CACHE = [
