13da365ee6b3ff25e420a6a385f2bbcb94ab66d4
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-06-11 20:21:24 +0000

    Update sw-beta.js
    Updated to version 2.0.0 with swipe-tap to close sidebar.

--- sw-beta.js
+++ sw-beta.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION  = "v1.4.1";
+const APP_VERSION  = "v2.0.0";
 const CACHE_NAME   = `alatipha-ges-pasco-${APP_VERSION}`;
 
 const FILES_TO_CACHE = [
