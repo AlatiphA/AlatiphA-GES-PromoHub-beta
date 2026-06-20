138957509c8d794b955ee73d617dca98eac54ef6
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-06-20 13:06:22 +0000

    Update app.js

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ let controlsVisible =
 let fontSize =
   Number(
     localStorage.getItem(
-      "fontSize-beta"
+      "fontSize"
     )
   ) || 100;
 
@@ -138,7 +138,7 @@ let fontSize =
    APP VERSION
    Change this on every release
 ========================= */
-const APP_VERSION = "3.3.8";
+const APP_VERSION = "3.0.0";
 
 const versionEl =
   document.getElementById(
@@ -149,10 +149,10 @@ if (versionEl)
     "v" + APP_VERSION;
 
 const READER_DATA_KEY =
-  "epub-beta-reader-data";
+  "ges-pasco-beta-data";
 
 const BOOKMARKS_KEY =
-  "epub-beta-bookmarks";
+  "ges-pasco-beta-bookmarks";
 
 
 /* =========================
@@ -662,10 +662,6 @@ function buildTOC(
 }
 
 
-/* =================
-   START READER
-================= */
-
 /* =========================
    SLIDE PAGE ANIMATION
 ========================= */
@@ -732,6 +728,11 @@ function pagePrev() {
   if (!rendition || _sliding) return;
   slidePage("prev", () => rendition.prev());
 }
+
+
+/* =================
+   START READER
+================= */
 
 function startReader() {
 
@@ -1702,7 +1703,7 @@ bottomDecreaseFont.addEventListener(
     );
 
     localStorage.setItem(
-      "fontSize-beta",
+      "fontSize",
       fontSize
     );
 
@@ -1720,7 +1721,7 @@ bottomIncreaseFont.addEventListener(
     );
 
     localStorage.setItem(
-      "fontSize-beta",
+      "fontSize",
       fontSize
     );
 
