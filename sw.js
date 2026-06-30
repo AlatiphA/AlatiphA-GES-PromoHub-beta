7ad61c50b8ce44791ee31efe876c8125c7739146
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-06-30 05:40:33 +0000

    Update app version to v1.2.1

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION  = "v1.2.0";
+const APP_VERSION  = "v1.2.1";
 const CACHE_NAME   = `alatipha-ges-${APP_VERSION}`;
 
 /* ====================
