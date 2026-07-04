3a07d68df3dbf0294aeb25dda8c244cd7a698171
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-07-04 06:16:05 +0000

    Update app version to 1.3.0

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ let fontSize =
    APP VERSION
    Change this on every release
 ========================= */
-const APP_VERSION = "1.2.6";
+const APP_VERSION = "1.3.0";
 
 const versionEl =
   document.getElementById(
