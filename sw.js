7c50f23b7538c046746d685a8ff6e6adc17f3790
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-07-06 20:55:22 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION  = "v1.3.7";
+const APP_VERSION  = "v1.3.8";
 const CACHE_NAME   = `alatipha-ges-promohub-${APP_VERSION}`;
 
 /* ====================
