751313dda680954cb915056823df09d483df1e4f
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-07-09 12:04:22 +0000

    Update app version to v1.3.14

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION  = "v1.3.13";
+const APP_VERSION  = "v1.3.14";
 const CACHE_NAME   = `alatipha-ges-promohub-${APP_VERSION}`;
 
 /* ====================
