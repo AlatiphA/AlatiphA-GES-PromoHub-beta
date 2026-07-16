11f120d0bcec81198c8eddbfa7a18e39ad4650df
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-07-16 14:18:50 +0000

    Update APP_VERSION to 1.4.3

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ let fontFamily =
    APP VERSION
    Change this on every release
 ========================= */
-const APP_VERSION = "1.4.2";
+const APP_VERSION = "1.4.3";
 
 const versionEl =
   document.getElementById(
